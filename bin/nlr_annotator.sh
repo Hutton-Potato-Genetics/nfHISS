f89ec231a3dc6131179ccf7772e832905aae7514
Thomas Adams <thomas.adams@hutton.ac.uk>
2025-02-06 17:10:53 +0000

    Corrects splitting of arguments

--- bin/nlr_annotator.sh
+++ bin/nlr_annotator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java $1 -jar "$(dirname "$0")/NLR-Annotator.jar" "$@:2"
+java $1 -jar "$(dirname "$0")/NLR-Annotator.jar" "${@:2}"
--- bin/nlr_parser.sh
+++ bin/nlr_parser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 MEME_XML="$(dirname "$0")/meme.xml"
 MAST="$(which mast)"
-java $1 -jar "$(dirname "$0")/NLR-Parser3.jar" -y $MAST -x $MEME_XML "$@:2"
+java $1 -jar "$(dirname "$0")/NLR-Parser3.jar" -y $MAST -x $MEME_XML "${@:2}"
